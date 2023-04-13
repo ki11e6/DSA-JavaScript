af92002b0201cc59e5a2fdd107a944f7f802042e
sharath <sharath.surendran.m@gmail.com>
2023-04-14 01:18:16 +0530

    hash table

---
+++ Problems/HashTable/findCommonInTwoArray.js
@@ -0,0 +1,14 @@
+function itemInCommon(arr1, arr2) {
+    let obj = {};
+    for (let i = 0; i < arr1.length; i++) {
+        obj[arr1[i]] = true;
+    }
+    for (let j = 0; j < arr2.length; j++) {
+        if (obj.hasOwnProperty(arr2[j])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(itemInCommon([1, 2, 3], [6, 7, 4, 33]));
