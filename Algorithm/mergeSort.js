5f9847ae3b4b9b2280829fbeea456a53f800ed4a
sharath <sharath.surendran.m@gmail.com>
2023-04-15 12:59:38 +0530

    insertion merge sort

---
+++ Algorithm/mergeSort.js
@@ -0,0 +1,25 @@
+function mergeSort(arr1, arr2) {
+    let combined = [];
+    let i = 0;
+    let j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            combined.push(arr1[i]);
+            i++;
+        } else {
+            combined.push(arr2[j]);
+            j++;
+        }
+    }
+    while (i < arr1.length) {
+        combined.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        combined.push(arr2[j]);
+        j++;
+    }
+    return combined;
+}
+
+console.log(mergeSort([1, 3, 7, 8], [2, 4, 5, 6]));
