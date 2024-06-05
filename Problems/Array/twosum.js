f9cca9bfbb174bb00db06aed501f6c33aa9e44eb
ki11e6 <sharath.surendran.m@gmail.com>
2024-06-05 20:59:33 +0530

    2Sum hash way solv

--- Problems/Array/twosum.js
+++ Problems/Array/twosum.js
@@ -2,14 +2,23 @@ let nums = [1, 3, 7, 9, 2];
 let target = 11;
 const twoSum = function (nums, target) {
     const obj = {};
+    //! way #1
+    // for (let i = 0; i < nums.length; i++) {
+    //     const curObjVal = obj[nums[i]];
+    //     if (curObjVal >= 0) {
+    //         return [curObjVal, i];
+    //     } else {
+    //         const NumToFind = target - nums[i];
+    //         obj[NumToFind] = i;
+    //     }
+    // }
+    //! way #2
     for (let i = 0; i < nums.length; i++) {
-        const curObjVal = obj[nums[i]];
-        if (curObjVal >= 0) {
-            return [curObjVal, i];
-        } else {
-            const NumToFind = target - nums[i];
-            obj[NumToFind] = i;
+        const numFind = target - nums[i];
+        if (obj.hasOwnProperty(numFind)) {
+            return [obj[numFind], i];
         }
+        obj[nums[i]] = i;
     }
     return null;
 };
